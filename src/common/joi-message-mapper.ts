511afd040559a9032a4a44140dc840eaaada1aa6
Abdel-Maliki <malikiamadou00@gmail.com>
2020-12-07 12:04:53 +0000

    router refactoring

--- src/common/joi-message-mapper.ts
+++ src/common/joi-message-mapper.ts
@@ -1,41 +1,11 @@
-import {Joi as JOI, OutputValidation} from 'koa-joi-router';
-import {JoiObject, ObjectSchema, SchemaMap} from "joi";
-import {MessageError, ModifiedContext} from "index";
+import {MessageError} from "index";
 
-enum methods {
-    POST = 'post',
-    GET = 'get',
-    DELETE = 'delete',
-    PUT = 'put'
-}
+/**
+ * @author abdel-maliki
+ * Date : 07/12/2020
+ */
 
-enum contentType {
-    JSON = 'json'
-}
-
-type defaultObject = { length: number };
-
-const defaults: defaultObject = {
-    length: 255
-};
-
-class Helper {
-    public static defaults = defaults;
-    public static methods = methods;
-    public static contentType = contentType;
-    public static mongoObjectRegEx = /^[a-f\d]{24}$/i;
-
-    public static validation = async (ctx: ModifiedContext, next: Function) => {
-        if (ctx.invalid) {
-            let body: { details: Array<MessageError> } = ctx.invalid.body || ctx.invalid.query || ctx.invalid.params;
-            let response: Array<MessageError | undefined | null> = body && body.details ? body.details : [];
-            const message: any = (response.length > 0 && response[0].hasOwnProperty('context'))
-                ? Helper.messageWrapper(response[0]) : response
-            ctx.answerUserError(412, message)
-        } else {
-            return await next();
-        }
-    };
+class JoiMessageMapper {
 
     public static numberMessageWrapper(messageError: MessageError) {
         return (messageError.type === 'number.max')
@@ -102,9 +72,9 @@ class Helper {
         return (messageError.type.includes('required'))
             ? `${messageError.context.label} est requis${messageError.context.label.toLowerCase().startsWith('la') ? 'e' : ''}`
             : messageError.type.startsWith('number')
-                ? Helper.numberMessageWrapper(messageError)
+                ? JoiMessageMapper.numberMessageWrapper(messageError)
                 : messageError.type.startsWith('string')
-                    ? Helper.stringMessageWrapper(messageError)
+                    ? JoiMessageMapper.stringMessageWrapper(messageError)
                     : (messageError.type.endsWith('empty'))
                         ? `${messageError.context.label} n'est pas autorisé à être vide`
                         : messageError.type.includes('unique')
@@ -114,112 +84,6 @@ class Helper {
         /*if (messageError.type.includes('max'))
             return `La taille maximale pour ${messageError.context.label} est ${messageError.context.limit}`*/
     };
-
-    public static validationErrorResponse() {
-        return Helper.errorResponse(412);
-    };
-
-    public static errorResponse(code: number) {
-        return {
-            [code]: {
-                body: {
-                    code,
-                    error: JOI.object({
-                        message: JOI.string()
-                    })
-                }
-            }
-        };
-    };
-
-    public static allErrorResponse() {
-        return {
-            '400-402,404-599': {
-                body: {
-                    code: JOI.number().min(400).max(599).required(),
-                    error: JOI.object({
-                        message: JOI.string()
-                    })
-                }
-            },
-            '403': {
-                body: {
-                    code: JOI.number().allow(403).required(),
-                    error: JOI.object({
-                        message: JOI.string()
-                    }),
-                    data: JOI.object({
-                        user: JOI.object(Helper.userOutput),
-                        roles: JOI.array().items(JOI.string()).allow([]),
-                    })
-                }
-            }
-        };
-    }
-
-    public static readonly userOutput: SchemaMap = {
-        id: JOI.string(),
-        email: JOI.string(),
-        name: JOI.string(),
-        userName: JOI.string(),
-        status: JOI.string(),
-        createdAt: JOI.date(),
-        profile: JOI.object({
-            id: JOI.string().allow(''),
-            name: JOI.string().allow(''),
-            description: JOI.string().allow(''),
-        })
-    }
-
-    public static successResponse<T = any>(code: number, data: T): { [status: string]: OutputValidation } {
-        return {
-            [code]: {
-                body: {
-                    code,
-                    data,
-                    PaginationDetails: JOI.object({
-                        page: JOI.number().optional(),
-                        size: JOI.number().optional(),
-                        totalElements: JOI.number().optional()
-                    }).optional(),
-                }
-            }
-        };
-    };
-
-    public static allSuccessResponse<T extends JoiObject>(data: T, pagination = false): { [status: string]: OutputValidation } {
-        return {
-
-            '200-299': {
-                body: JOI.object({
-                    code: JOI.number().min(200).max(299).required(),
-                    data,
-                    pagination: pagination ? Helper.paginationData().required() : Helper.paginationData().optional(),
-                }).options({stripUnknown: true})
-            }
-        };
-    }
-
-    public static paginationData<T = any>(): ObjectSchema {
-        return JOI.object({
-            page: JOI.number().required(),
-            size: JOI.number().required(),
-            sort: JOI.string().required(),
-            direction: JOI.number().equal([1, -1]),
-            totalElements: JOI.number().required(),
-            globalFilter: JOI.string(),
-            filters: JOI.object()
-        });
-    }
-
-
-    public static defaultOutput<T extends JoiObject>(data: T, pagination = false): { [status: string]: OutputValidation } {
-
-        return Object.assign({},
-            Helper.allErrorResponse(),
-            Helper.allSuccessResponse<T>(data, pagination),
-        )
-    }
 }
 
-export default Helper;
+export default JoiMessageMapper;
